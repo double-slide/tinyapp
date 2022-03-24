f7aa93b06071e32e54d22f1e3556acaa2eb68fe6
double-slide <93881358+double-slide@users.noreply.github.com>
2022-03-24 21:17:59 +0000

    Added login page, removed login form from header, added check for duplicate register, added check for correct email/pw at login

--- express_server.js
+++ express_server.js
@@ -8,91 +8,42 @@ const morgan = require('morgan');
 const bodyParser = require("body-parser");
 let cookieParser = require("cookie-parser");
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
 app.use(cookieParser());
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
 
-app.get("/", (req, res) => {
-  res.redirect("/urls");
-});
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
 
-app.get("/urls", (req, res) => {
-  const templateVars = { 
-    username: req.cookies["username"],
-    urls: urlDatabase
-  };
-  res.render("urls_index", templateVars);
-});
+// PSEUDO DATABASES
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-app.get("/urls/new", (req, res) => {
-  const templateVars = { 
-    username: req.cookies["username"],
-  };
-  res.render("urls_new", templateVars);
-});
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
-app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { 
-    username: req.cookies["username"],
-    shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL]
-  };
-  res.render("urls_show", templateVars);
-});
+const users = {
+  '123456': {
+    id: "123456", 
+    email: "user123456@example.com", 
+    password: "123456-purple-monkey-dinosaur"
+  },
+  '654321': {
+    id: "654321", 
+    email: "b@b.com", 
+    password: "b"
+  }
+};
 
-app.post("/urls", (req, res) => {
-  shortURL = generateRandomString()
-  longURL = req.body.longURL;
-  urlDatabase[shortURL] = longURL;
-  res.redirect(`urls/${shortURL}`);
-});
 
-app.post("/urls/:shortURL/delete", (req, res) => {
-  delete urlDatabase[req.params.shortURL];
-  res.redirect("/urls");
-});
 
-app.post("/login", (req, res) => {
-  let cookieForUser = req.body.username;
-  res.cookie('username', cookieForUser);
-  res.redirect("/urls");
-});
 
-app.post("/logout", (req, res) => {
-  // let cookieForUser = req.body.username;
-  res.clearCookie('username');
-  res.redirect("/urls");
-});
-
-app.post("/urls/:shortURL/update", (req, res) => {
-  urlDatabase[req.params.shortURL] = req.body.longURL;
-  res.redirect("/urls");
-});
-
-app.post("/urls/:shortURL/edit", (req, res) => {
-  shortURL = req.params.shortURL;
-  console.log("shortURL:",shortURL)
-  res.redirect(`/urls/${shortURL}`);
-});
-
-app.get("/u/:shortURL", (req, res) => {
-  shortURL = req.params.shortURL;
-  longURL = urlDatabase[shortURL];
-  res.redirect(longURL);
-});
+// HELPER FUNCTIONS
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const generateRandomString = function() {
   let characters = '0123456789abcdeghijklmnopqrstuvwxyz';
@@ -104,3 +55,220 @@ const generateRandomString = function() {
   return randomUrl;
 };
 
+const emailRegisteredAlready = function(candidateEmail) { 
+  for (let property in users) {
+    if (users[property].email === candidateEmail) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const getUserIdFromEmail = function(verifiedEmail) {
+  for (let property in users) {
+    if (users[property].email === verifiedEmail) {
+      return users[property].id;
+    }
+  }
+  console.log("Email was not found in users object");
+  return;
+}
+
+const isPasswordCorrectForEmail = function(verifiedEmail, candidatePassword) {
+  for (let property in users) {
+    if (users[property].email === verifiedEmail && users[property].password == candidatePassword) {
+      return true;
+    }
+  }
+
+  console.log("Wrong password submitted");
+  return false;
+};
+
+
+// GET FUNCTIONS
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+
+app.get("/", (req, res) => {
+  res.redirect("/urls");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/urls", (req, res) => {
+  
+  let user_id = req.cookies["user_id"]
+  let user = users[user_id];
+  const templateVars = { 
+    user_id: user,
+    urls: urlDatabase
+  };
+  
+  res.render("urls_index", templateVars);
+  // res.render("urls_index");
+});
+
+app.get("/urls/new", (req, res) => {
+  let user_id = req.cookies["user_id"]
+  let user = users[user_id];
+  const templateVars = { 
+    user_id: user,
+    urls: urlDatabase
+  };
+  res.render("urls_new", templateVars);
+});
+
+
+app.get("/urls/:shortURL", (req, res) => {
+  let user_id = req.cookies["user_id"]
+  let user = users[user_id];
+  // console.log("req.params.shortURL", req.params.shortURL);
+  let shortURL = req.params.shortURL;
+  let longURL = urlDatabase[shortURL];
+  const templateVars = { 
+    user_id: user,
+    urls: urlDatabase,
+    longURL: longURL,
+    shortURL: shortURL
+  };  
+  res.render("urls_show", templateVars);
+});  
+
+
+app.get("/register", (req, res) => {
+  
+  let user_id = req.cookies["user_id"]
+  let user = users[user_id];
+  const templateVars = { 
+    user_id: user,
+    urls: urlDatabase
+  };
+  
+  
+  res.render("user_register", templateVars);
+});  
+
+
+app.get("/u/:shortURL", (req, res) => {
+  shortURL = req.params.shortURL;
+  longURL = urlDatabase[shortURL];
+  res.redirect(longURL);
+});
+
+
+app.get("/login", (req,res) => {
+  let user_id = req.cookies["user_id"]
+  let user = users[user_id];
+  const templateVars = { 
+    user_id: user,
+    urls: urlDatabase
+  };
+  res.render("user_login", templateVars);
+});
+
+
+
+
+// POST FUNCTIONS
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+app.post("/register", (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  
+  if (email === "" || password === "") {
+    res.send("Error 400 - Bad Request, empty email or password field");
+    console.log("users", users);
+    return;
+  }
+  
+  if (emailRegisteredAlready(email)) {
+    res.send("Error 400 - Bad Request, email already registered");
+    console.log("users", users);
+    return;
+  }
+
+  user_id = generateRandomString();
+  
+  users[user_id] = {
+    id: user_id,
+    email: email,
+    password: password
+  }  
+  // console.log("users:", users);
+  res.cookie('user_id', user_id);
+  res.redirect("/urls");
+});  
+
+
+app.post("/urls", (req, res) => {
+  shortURL = generateRandomString()
+  longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`urls/${shortURL}`);
+});  
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+});  
+
+app.post("/login", (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  
+  if (email === "" || password === "") {
+    res.send("Error 400 - Bad Request, empty email or password field");
+    return;
+  }
+  
+  if (!emailRegisteredAlready(email)) {
+    res.send("Error 403 - Forbidden, email not registered");
+    return;
+  }
+
+  if (!isPasswordCorrectForEmail(email, password)) {
+    res.send("Error 403 - Forbidden, no credential match found");
+    return;
+  }
+
+  // plant cookie
+  user_id = getUserIdFromEmail(email);
+
+  console.log("user_id", user_id);
+  console.log("typeof user_id", typeof user_id);
+
+
+
+
+
+  res.cookie('user_id', user_id);
+  res.redirect("/urls");
+});  
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('user_id');
+  res.redirect("/urls");
+});  
+
+app.post("/urls/:shortURL/update", (req, res) => {
+  urlDatabase[req.params.shortURL] = req.body.longURL;
+  res.redirect("/urls");
+});  
+
+app.post("/urls/:shortURL/edit", (req, res) => {
+  shortURL = req.params.shortURL;
+  console.log("shortURL:",shortURL)
+  res.redirect(`/urls/${shortURL}`);
+});
