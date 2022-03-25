f570cb85d64c8def658035143de9a4970279302a
double-slide <93881358+double-slide@users.noreply.github.com>
2022-03-25 00:38:03 +0000

    Added user authentification for edit/delete routes, and added error html pages when attempting to access while logged out

--- express_server.js
+++ express_server.js
@@ -7,6 +7,7 @@ const morgan = require('morgan');
 
 const bodyParser = require("body-parser");
 let cookieParser = require("cookie-parser");
+const res = require("express/lib/response");
 
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -15,21 +16,25 @@ app.use(cookieParser());
 
 
 
-
 // PSEUDO DATABASES
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    user_id: "654321"
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    user_id: "123456"
+  }
 };
 
 const users = {
   '123456': {
     id: "123456", 
-    email: "user123456@example.com", 
-    password: "123456-purple-monkey-dinosaur"
+    email: "a@a.com", 
+    password: "a"
   },
   '654321': {
     id: "654321", 
@@ -40,10 +45,8 @@ const users = {
 
 
 
-
 // HELPER FUNCTIONS
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////
 
 const generateRandomString = function() {
   let characters = '0123456789abcdeghijklmnopqrstuvwxyz';
@@ -85,11 +88,25 @@ const isPasswordCorrectForEmail = function(verifiedEmail, candidatePassword) {
   return false;
 };
 
+const urlsForUser = function(idToFilter) {
+  console.log("urlDatabase:",urlDatabase);
+  const filteredUrlObject = {};
+  const keys = Object.keys(urlDatabase);
+  for (const shortURL of keys) {
+    const url = urlDatabase[shortURL];
+    if (url.user_id === idToFilter) {
+      filteredUrlObject[shortURL] = url;
+    }
+  }
+  console.log("filteredUrlObject:",filteredUrlObject);
 
-// GET FUNCTIONS
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  return filteredUrlObject;
+}
 
+
+
+// ROUTES
+////////////////////////////////
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
@@ -100,67 +117,75 @@ app.get("/", (req, res) => {
   res.redirect("/urls");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
 
 app.get("/urls", (req, res) => {
-  
   let user_id = req.cookies["user_id"]
+  if (!user_id) {
+    res.send("Please <a href='/login'>login</a> to view URLs!")
+    return;
+  }
+  const urls = urlsForUser(user_id);
   let user = users[user_id];
   const templateVars = { 
     user_id: user,
-    urls: urlDatabase
+    urls: urls
   };
-  
   res.render("urls_index", templateVars);
-  // res.render("urls_index");
 });
+
 
 app.get("/urls/new", (req, res) => {
   let user_id = req.cookies["user_id"]
+  if (!user_id) {
+    res.send("Please <a href='/login'>login</a> to create a new URL!")
+    return;
+  }
   let user = users[user_id];
   const templateVars = { 
-    user_id: user,
-    urls: urlDatabase
-  };
+  user_id: user,
+  urls: urlDatabase
+  }
   res.render("urls_new", templateVars);
 });
 
 
 app.get("/urls/:shortURL", (req, res) => {
   let user_id = req.cookies["user_id"]
+  if (user_id === undefined){
+    res.send("Please <a href='/login'>login</a> to view and edit your URLs!");
+    return
+  }
   let user = users[user_id];
-  // console.log("req.params.shortURL", req.params.shortURL);
   let shortURL = req.params.shortURL;
-  let longURL = urlDatabase[shortURL];
+  let longURL = urlDatabase[shortURL].longURL;
+
   const templateVars = { 
     user_id: user,
     urls: urlDatabase,
     longURL: longURL,
     shortURL: shortURL
-  };  
+  };
   res.render("urls_show", templateVars);
 });  
 
 
 app.get("/register", (req, res) => {
-  
   let user_id = req.cookies["user_id"]
   let user = users[user_id];
   const templateVars = { 
     user_id: user,
     urls: urlDatabase
   };
-  
-  
   res.render("user_register", templateVars);
 });  
 
 
 app.get("/u/:shortURL", (req, res) => {
   shortURL = req.params.shortURL;
-  longURL = urlDatabase[shortURL];
+  if (!urlDatabase[shortURL]) {
+    res.send("Sorry, short URL does not exist. Please try again!");
+  }
+  longURL = urlDatabase[shortURL].longURL;
   res.redirect(longURL);
 });
 
@@ -176,99 +201,105 @@ app.get("/login", (req,res) => {
 });
 
 
-
-
-// POST FUNCTIONS
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
 app.post("/register", (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
-  
   if (email === "" || password === "") {
     res.send("Error 400 - Bad Request, empty email or password field");
     console.log("users", users);
     return;
   }
-  
   if (emailRegisteredAlready(email)) {
     res.send("Error 400 - Bad Request, email already registered");
     console.log("users", users);
     return;
   }
-
   user_id = generateRandomString();
-  
   users[user_id] = {
     id: user_id,
     email: email,
     password: password
   }  
-  // console.log("users:", users);
   res.cookie('user_id', user_id);
   res.redirect("/urls");
 });  
 
 
 app.post("/urls", (req, res) => {
+  let user_id = req.cookies["user_id"]
+  
+  if (user_id === undefined){
+    res.send("Please <a href='/login'>login</a> to view!");
+    return;
+  }
   shortURL = generateRandomString()
-  longURL = req.body.longURL;
-  urlDatabase[shortURL] = longURL;
+  let newURLObject = {
+    longURL: req.body.longURL,
+    user_id: user_id
+  }
+
+  urlDatabase[shortURL] = newURLObject;
   res.redirect(`urls/${shortURL}`);
 });  
 
+
 app.post("/urls/:shortURL/delete", (req, res) => {
+  let user_id = req.cookies["user_id"]
+  if (user_id === undefined){
+    res.send("Please <a href='/login'>login</a> to edit!");
+    return;
+  }
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
 });  
 
+
 app.post("/login", (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
-  
   if (email === "" || password === "") {
     res.send("Error 400 - Bad Request, empty email or password field");
     return;
   }
-  
   if (!emailRegisteredAlready(email)) {
     res.send("Error 403 - Forbidden, email not registered");
     return;
   }
-
   if (!isPasswordCorrectForEmail(email, password)) {
     res.send("Error 403 - Forbidden, no credential match found");
     return;
   }
-
-  // plant cookie
   user_id = getUserIdFromEmail(email);
-
   console.log("user_id", user_id);
   console.log("typeof user_id", typeof user_id);
-
-
-
-
-
   res.cookie('user_id', user_id);
   res.redirect("/urls");
 });  
+
 
 app.post("/logout", (req, res) => {
   res.clearCookie('user_id');
   res.redirect("/urls");
 });  
 
+
 app.post("/urls/:shortURL/update", (req, res) => {
-  urlDatabase[req.params.shortURL] = req.body.longURL;
+  let user_id = req.cookies["user_id"]
+  if (user_id === undefined){
+    res.send("Please <a href='/login'>login</a> to edit!");
+    return;
+  }
+  const shortURL = req.params.shortURL;
+  let newURLObject = {
+    longURL: req.body.longURL,
+    user_id: user_id
+  }
+  urlDatabase[shortURL] = newURLObject;
   res.redirect("/urls");
 });  
 
+
 app.post("/urls/:shortURL/edit", (req, res) => {
   shortURL = req.params.shortURL;
-  console.log("shortURL:",shortURL)
   res.redirect(`/urls/${shortURL}`);
 });
